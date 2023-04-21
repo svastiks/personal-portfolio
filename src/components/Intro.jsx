779e19914f132b1439cfdfa3f0b1183227ea5425
Svastik Sharma <svastik@my.yorku.ca>
2023-04-20 21:18:35 -0400

    multiple components created

---
+++ src/components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home(){
+    return(
+        <main>
+        <div id = "introduction">
+            <div className="name">
+                Svastik Sharma
+            </div>
+
+            <div className="profession">
+                SWE and Student
+            </div>
+        </div>
+        </main>
+    )
+}
---
+++ src/components/Skills.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default function Skills() {
+    <div className="skillsTotal">
+        <div className="languages">
+            <div className="skill">
+                Java
+            </div>
+
+            <div className="skill">
+                JavaScript
+            </div>
+
+            <div className="skill">
+                C++
+            </div>
+
+            <div className="skill">
+                
+            </div>
+
+            <div className="skill">
+
+            </div>
+        </div>
+
+        <div className="frameworksTotal">
+
+            <div className="framework">
+
+            </div>
+
+            <div className="framework">
+
+            </div>
+
+            <div className="framework">
+
+            </div>
+
+            <div className="framework">
+
+            </div>
+        </div>
+    </div>
+}
