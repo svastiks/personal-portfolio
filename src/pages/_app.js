72bb351a2308abac046c634cbcf4ef5cee676f88
Svastik Sharma <svastiksharma13@gmail.com>
2024-02-19 00:22:36 -0500

    update

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '@/styles/globals.css'
-import '@/styles/stylesheet.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
