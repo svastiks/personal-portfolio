bffcf80d01becc06b2a81cfc357731a5f1282f7f
Svastik Sharma <svastiksharma13@gmail.com>
2024-03-05 22:16:02 -0500

    update

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -6,9 +6,9 @@ const Intro = () => {
 
             <h1 className='text-4x1 md:text-7x1 mb-1 md:mb-3 font-bold'>Svastik Sharma</h1>
 
-            <p>Software Engineering Student</p>
+            <p className='text-base md:text-xl mb-3 font-medium'>Software Engineering Student</p>
 
-            <p> Working! </p>
+            <p className='text-sm max-w-xl mb-6 font-bold'> Working! </p>
 
         </div>
     )
