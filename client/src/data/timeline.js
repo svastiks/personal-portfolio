e9b40bbbdeeab03c524db0bc49ca5ca64e6f32f3
Svastik Sharma <svastiksharma13@gmail.com>
2024-03-09 23:30:47 -0500

    add line break

--- client/src/data/timeline.js
+++ client/src/data/timeline.js
@@ -20,10 +20,10 @@ export default [
 		details:
 
 		`
-		- Acted as a lead intern on a project which helped improve my management and leadership skills on a large scale.
-		- Implemented new workflows using JavaScript, jQuery, and CakePHP and seamlessly integrated resulting API responses.
-		- Utilised SQL for retrieving and updating configurations and client data in both development and QA databases.
-		- Handled minor production issues after data migration on both the client and server side in an efficient time frame.
+		- Acted as a lead intern on a project which helped improve my management and leadership skills on a large scale. \n
+		- Implemented new workflows using JavaScript, jQuery, and CakePHP and seamlessly integrated resulting API responses. \n
+		- Utilised SQL for retrieving and updating configurations and client data in both development and QA databases. \n
+		- Handled minor production issues after data migration on both the client and server side in an efficient time frame. \n
 		
 		`
 	},
@@ -34,10 +34,10 @@ export default [
 		details:
 		`
 
-		- Led end-to-end revamping of an existing project, increasing app engagement, and implemented new services using JavaScript, jQuery, Bootstrap and CakePHP.
-		- Retrieved data from RESTful APIs using HTTP methodology, handling responses, parsing data i.e. XML to JSON, and optimising performance to ensure secure data retrieval.
-		- Reduced page loading times by 30% and improved responsiveness by upgrading frameworks and libraries.
-		- Conducted detailed software testing in development and QA environments leading to major bug fixes and workflow corrections before prod deployment, resulting in minimal production issues.	
+		- Led end-to-end revamping of an existing project, increasing app engagement, and implemented new services using JavaScript, jQuery, Bootstrap and CakePHP. \n
+		- Retrieved data from RESTful APIs using HTTP methodology, handling responses, parsing data i.e. XML to JSON, and optimising performance to ensure secure data retrieval. \n
+		- Reduced page loading times by 30% and improved responsiveness by upgrading frameworks and libraries. \n
+		- Conducted detailed software testing in development and QA environments leading to major bug fixes and workflow corrections before prod deployment, resulting in minimal production issues. \n
 		
 		`
 	}
