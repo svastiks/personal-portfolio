39a603e22dcc1089553a378d0897ca88604ab792
Svastik Sharma <svastiksharma13@gmail.com>
2024-03-09 23:29:06 -0500

    update

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Intro = () => {
-    return (
-        <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+function Intro() {
+   return (
+      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Svastik Sharma</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">Software Engineering Student</p>
+         <p className="text-sm max-w-xl mb-6 font-bold">
+            {' '}
 
-            <h1 className='text-4x1 md:text-7x1 mb-1 md:mb-3 font-bold'>Svastik Sharma</h1>
-
-            <p className='text-base md:text-xl mb-3 font-medium'>Software Engineering Student</p>
-
-            <p className='text-sm max-w-xl mb-6 font-bold'> Currently working @ theScore</p>
-
-        </div>
-    )
+         </p>
+      </div>
+   )
 }
 
-export default Intro
+export default Intro;
---
+++ client/src/data/timeline.js
@@ -0,0 +1,44 @@
+export default [
+   {
+      year: 'Sept 2023 - Present',
+      title: 'Software QA Engineer Intern | CO-OP',
+      duration: 'theScore',
+      details: `
+	 	
+	- Created and executed regression test plans and automation test scripts to support the ESPN Bet release window. \n
+	- Developed new and updated existing automation test cases using Kotlin, TestNG, Selenium Web Driver and Cucumber. \n
+	- Reduced manual testing by 15% due to automation test implementation \n
+	- Utilised Jenkins for continuous integration by creating deployment jobs to execute automation tests. \n
+	- Performed cross-functional testing for theScore Bet and the ESPN Bet on production and development builds. \n 
+	 
+	  `
+   },
+   {
+		year: 'May 2023 - Aug 2023',
+		title: 'Full Stack Developer Intern | CO-OP',
+		duration: '4Pay Inc.',
+		details:
+
+		`
+		- Acted as a lead intern on a project which helped improve my management and leadership skills on a large scale.
+		- Implemented new workflows using JavaScript, jQuery, and CakePHP and seamlessly integrated resulting API responses.
+		- Utilised SQL for retrieving and updating configurations and client data in both development and QA databases.
+		- Handled minor production issues after data migration on both the client and server side in an efficient time frame.
+		
+		`
+	},
+	{
+		year: 'Jan 2023 - Apr 2023',
+		title: 'Full Stack Developer Intern | CO-OP',
+		duration: '4Pay Inc.',
+		details:
+		`
+
+		- Led end-to-end revamping of an existing project, increasing app engagement, and implemented new services using JavaScript, jQuery, Bootstrap and CakePHP.
+		- Retrieved data from RESTful APIs using HTTP methodology, handling responses, parsing data i.e. XML to JSON, and optimising performance to ensure secure data retrieval.
+		- Reduced page loading times by 30% and improved responsiveness by upgrading frameworks and libraries.
+		- Conducted detailed software testing in development and QA environments leading to major bug fixes and workflow corrections before prod deployment, resulting in minimal production issues.	
+		
+		`
+	}
+]
