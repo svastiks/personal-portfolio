155e3dd552b4b0074ee4df941e23453fe38f0302
Svastik Sharma <svastiksharma13@gmail.com>
2024-03-10 16:20:37 -0400

    updated intro

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -7,14 +7,20 @@ function Intro() {
             
             <div className=''>   
                <div className = 'flex justify-center items-center'>
-                  <img src='/assets/pfp.jpg' width={200} height={200} style={{borderRadius : '30%'}}></img>
+                  <img src='/assets/pfp.jpg' width={200} height={200} style={{borderRadius : '35%'}}></img>
                </div>
 
                Svastik Sharma
             </div>
 
          </h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineering Student and Intern <a href='https://www.thescore.com/' target='_blank'>@theScore</a></p>
+         <p className="text-base md:text-xl mb-3 font-medium">
+            Software Engineering Student and Intern 
+            <a href='https://www.thescore.com/' target='_blank' style={{ display: 'inline', verticalAlign: 'middle' }}>
+            
+            @ theScore <img src='/assets/theScore.png' width={100} height={100} style={{ display: 'inline-block', verticalAlign: 'middle' }}></img>
+
+            </a></p>
        
       </div>
    )
